96dcd9803ab0c169c91a604b3f9e5ddfb5c8b561
tanaynaphade <61400075+tanaynaphade@users.noreply.github.com>
2024-11-08 14:26:32 +0530

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,55 +1,57 @@
 import java.util.Scanner;
 
-public class RoundRobin{
-	int burst[], run[], np, quantum = 0, wait[], time = 0, rp = 0, ta[];
-	RoundRobin() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of processes: ");
-		np = sc.nextInt();
-		rp = np;
-		burst = new int[np];
-		run = new int[np];
-		wait = new int[np];
-		ta = new int[np];
-		System.out.println("Enter their burst times: ");
-		for (int i = 0; i < np; i++) {
-			burst[i] = sc.nextInt();
-			run[i] = burst[i];
-			wait[i] = 0;
-		}
-		System.out.println("Enter the quantum time: ");
-		quantum = sc.nextInt();
+public class RoundRobin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+        
+        int[] burstTime = new int[n];
+        int[] remainingTime = new int[n];
+        int[] waitingTime = new int[n];
+        int[] turnaroundTime = new int[n];
+
+        System.out.print("Enter the time quantum: ");
+        int timeQuantum = sc.nextInt();
+        
+        System.out.println("Enter burst times for each process:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Process " + (i + 1) + ": ");
+            burstTime[i] = sc.nextInt();
+            remainingTime[i] = burstTime[i];
+        }
+
+        int currentTime = 0;
+        boolean done;
+        
+        do {
+            done = true;
+            for (int i = 0; i < n; i++) {
+                if (remainingTime[i] > 0) {
+                    done = false; 
+
+                    if (remainingTime[i] > timeQuantum) {
+                        currentTime += timeQuantum;
+                        remainingTime[i] -= timeQuantum;
+                    } else {
+                        currentTime += remainingTime[i];
+                        waitingTime[i] = currentTime - burstTime[i];
+                        remainingTime[i] = 0;
+                    }
+                }
+            }
+        } while (!done);
+
+        for (int i = 0; i < n; i++) {
+            turnaroundTime[i] = burstTime[i] + waitingTime[i];
+        }
+
+        System.out.println("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+        for (int i = 0; i < n; i++) {
+            System.out.println("P" + (i + 1) + "\t\t" + burstTime[i] + "\t\t" + waitingTime[i] + "\t\t" + turnaroundTime[i]);
+        }
+
         sc.close();
-		logic();
-	}
-	public void logic() {
-		int i = 0;
-		while (rp != 0) {
-			if (run[i] > quantum) {
-				run[i] -= quantum;
-				time += quantum;
-				System.out.println("Process: " + i + " ran for " + quantum + " units. Time: " + time);
-			} else if (run[i] <= quantum && run[i] > 0) {
-				time += run[i];
-				run[i] = 0;
-				ta[i] = time; // Completion time for process i
-				rp--; // Process finished
-				System.out.println("Process: " + i + " finished at time: " + time);
-			}
-			i++;
-			// Round-robin: if we reach the last process, go back to the first process
-			if (i == np) {
-				i = 0;
-			}
-		}
-		// Calculate waiting time and print results
-		System.out.println("\nProcess\tBurst\tTurnaround\tWaiting");
-		for (int j = 0; j < np; j++) {
-			wait[j] = ta[j] - burst[j]; // Waiting time = Turnaround time - Burst time
-			System.out.println(j + "\t" + burst[j] + "\t" + ta[j] + "\t\t" + wait[j]);
-		}
-	}
-	public static void main(String[] args) {
-		new RoundRobin();
-	}
+    }
 }
